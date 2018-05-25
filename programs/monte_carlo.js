81cb1177e6ba12aa8cd656e7b529baacdebd0892
vivek1729 <sasuke.vivek@gmail.com>
2018-05-25 11:50:27 -0700

    Added monte carlo source program in js and c

---
+++ programs/monte_carlo.js
@@ -0,0 +1,17 @@
+function mcpi(n) {
+    var x, y, m = 0;
+ 
+    for (var i = 0; i < n; i += 1) {
+        x = Math.random();
+        y = Math.random();
+ 
+        if (x * x + y * y < 1) {
+            m += 1;
+        }
+    }
+ 
+    return 4 * m / n;
+}
+ 
+
+console.log(mcpi(1000000));
